981378eb640884306ce2b83aec1bba3229083e66
Eternatus <anikroy173@gmail.com>
2023-05-17 15:53:23 +0530

    Update main.dart

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,37 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+  runApp(
+    //runApp is used to display everything on the app screen.
+    MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'Hello World!',
-              ),
-              Text(
-                'It\'s time to learn Flutter!',
-              ),
-            ],
+        //Scaffold is what contains the widgets describing the screen.
+        body: Container(
+          //wrapped with Container because we wanted a gradient background and it is not available otherwise.
+          decoration: const BoxDecoration(
+            //used to decorate the background in this case
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 8, 3, 95),
+                Color.fromARGB(255, 40, 4, 89)
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              "Hello World",
+              style: TextStyle(
+                  //no need to wrap text with another container, Text constructor function already has style parameter that can be used to style the text
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
